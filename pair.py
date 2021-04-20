154cf1953b9691a949c54c6065a0755a3e5bdccf
Claudiu <klaudyu@gmail.com>
2021-04-20 17:39:11 +0200

    pylinted the code

--- pair.py
+++ pair.py
@@ -1,13 +1,14 @@
+"""the name generator is a function that generates a name from an adjective and a noun"""
 import numpy as np
+
 def name_generator():
-	with open('nouns.txt') as f: nouns=f.readlines()
-	with open('adjectives.txt') as f: adj=f.readlines()
-	nouns,adj=[[x.strip() for x in dic] for dic in [nouns,adj]]
-	m='the '+' '.join([np.random.choice(d) for d in [adj,nouns]])
-	return m
+    """returns a random name constructed from an adjective and a noun"""
+    with open('nouns.txt') as ns_file:
+        nouns = ns_file.readlines()
+    with open('adjectives.txt') as ad_file:
+        adj = ad_file.readlines()
+    nouns, adj = [[x.strip() for x in dic] for dic in [nouns, adj]]
+    the_name = 'the '+' '.join([np.random.choice(d) for d in [adj, nouns]])
+    return the_name
 
 print(name_generator())
-
-
-
-
